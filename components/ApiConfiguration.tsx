5d463c235118c47d9565ecee2140009ad3c363a7
Jeremy Fenard <81246202+webexpr-jfenard@users.noreply.github.com>
2025-06-15 22:01:18 +0200

    Update ApiConfiguration.tsx

--- components/ApiConfiguration.tsx
+++ components/ApiConfiguration.tsx
@@ -5,7 +5,7 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Switch } from "./ui/switch";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
-import { toast } from "sonner@2.0.3";
+import { toast } from "sonner";
 import { configureRealAPI } from "../services/storageService";
 
 interface ApiConfigurationProps {
